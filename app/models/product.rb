a11228b001652bd808292f770ba62fb19827e614
MiroslavM <zerborus@hotmail.com>
2011-05-09 19:16:15 +0200

    added cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 class Product < ActiveRecord::Base
-default_scope :order => 'title'
+	default_scope :order => 'title'
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+	private
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+		 return true
+		else
+		 errors.add(:base, 'Line Items present')
+		 return false
+		end
+	end
+	
+	
 validates :title, :description, :image_url, :presence => true
 validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 validates :title, :uniqueness => true
