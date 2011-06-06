13f521f8d6293fbc237790dfca88d31360b88e74
Miroslav Mirkovic <mmirkovi@hsz-t.ch>
2011-06-06 19:05:34 +0200

    fixed cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,7 +43,6 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product.id)
-    @line_item = @cart.line_items.build(:product => product)
 
     respond_to do |format|
       if @line_item.save
